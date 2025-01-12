35f84cf7458331c958ea0b3aee52a476383876e2
Mateie <contact@mateie.dev>
2025-01-11 18:08:34 -0800

    added some tsdoc

--- index.ts
+++ index.ts
@@ -1,14 +1,27 @@
 import { z } from "zod";
 
+/**
+ * User Schema using Zod
+ */
 export const ZUser = z.object({
     id: z.string(),
     username: z.string(),
 });
 
+/**
+ * User with sensitive data Schema using Zod
+ */
 export const ZUserWithSensitiveData = ZUser.extend({
     email: z.string(),
     password: z.string(),
 });
 
+/**
+ * User type infered from Zod schema
+ */
 export type User = z.infer<typeof ZUser>;
+
+/**
+ * User with sensitive data type infered from Zod schema
+ */
 export type UserWithSensitiveData = z.infer<typeof ZUserWithSensitiveData>;
