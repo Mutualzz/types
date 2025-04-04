6da22270b7f2f23e3385d5ff51a50ea2e60999f7
mateie <mateie@gmail.com>
2025-04-04 14:22:19 -0700

    added opcodes types and a variable

---
+++ index.ts
@@ -0,0 +1,10 @@
+export const Opcodes = {
+    Dispatch: 0, // Dispatch opcode, used to send payloads like (message updates, server updates and etc.)
+    Heartbeat: 1, // Heartbeat opcode, used to check if the connection is alive
+    Identify: 2, // For authentication purposes, used to identify the client to the server
+    Resume: 3, // Used to resume a previous session, if the connection was lost
+    Reconnect: 4, // Instructs the client to reconnect to the server
+    InvalidSession: 5, // Indicates that the session is invalid or expired
+    Hello: 6, // Sent by the server to initiate the connection and provide the heartbeat interval
+    HeartbeatAck: 7, // Acknowledgment for the heartbeat sent by the client, indicating the server received the heartbeat
+};
