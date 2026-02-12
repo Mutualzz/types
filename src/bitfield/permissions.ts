30f9201ad67741111c6a71d7696a57302a436c7d
Azrael <azrael@mutualzz.com>
2026-02-12 15:01:59 -0800

    Fixing bugs and working on roles and permissions

--- src/bitfield/permissions.ts
+++ src/bitfield/permissions.ts
@@ -1,10 +1,22 @@
 export const permissionFlags = {
+    // Channels
     ViewChannel: 1n << 0n,
-    SendMessages: 1n << 1n,
-    ManageMessages: 1n << 2n,
-    ManageRoles: 1n << 3n,
-    KickMembers: 1n << 4n,
-    BanMembers: 1n << 5n,
+    ManageChannels: 1n << 1n,
+
+    // Messages
+    SendMessages: 1n << 2n,
+    ManageMessages: 1n << 3n,
+
+    // Roles
+    ManageRoles: 1n << 4n,
+
+    // Members
+    KickMembers: 1n << 5n,
+    BanMembers: 1n << 6n,
+
+    // Invites
+    CreateInvites: 1n << 7n,
+    ManageInvites: 1n << 8n,
 
     Administrator: 1n << 60n,
 } as const satisfies Record<string, bigint>;
