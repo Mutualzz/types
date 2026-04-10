9b077cb0351df948ced5b5ca42cf399a7f2073ea
Azrael <azrael@mutualzz.com>
2026-04-09 19:00:32 -0700

    Release time

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,1 +1,342 @@
-export * from "./v1";
+import type {
+    AppMode,
+    ChannelType,
+    InviteType,
+    MessageType,
+    Snowflake,
+    ThemeStyle,
+    ThemeType,
+} from "../common";
+
+// Theme types (we shouldn't export these individually, since we already do it in ui-core)
+type LinearGradient = `linear-gradient(${string})`;
+type RadialGradient = `radial-gradient(${string})`;
+type ConicGradient = `conic-gradient(${string})`;
+
+type Gradient = LinearGradient | RadialGradient | ConicGradient;
+
+type Hex = `#${string}`;
+type RGB = `rgb(${number}, ${number}, ${number})`;
+type RGBA = `rgba(${number}, ${number}, ${number}, ${number})`;
+type HSL = `hsl(${number}, ${number}%, ${number}%)`;
+type HSLA = `hsla(${number}, ${number}%, ${number}%, ${number})`;
+type HSV = `hsv(${number}, ${number}%, ${number}%)`;
+type HSVA = `hsva(${number}, ${number}%, ${number}%, ${number})`;
+
+type ColorLike = Hex | RGB | RGBA | HSL | HSLA | HSV | HSVA | Gradient;
+
+type TypographyDisplayKey =
+    | "display-xs"
+    | "display-sm"
+    | "display-md"
+    | "display-lg";
+type TypographyHeadingKey = "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+type TypographyTitleKey = "title-sm" | "title-md" | "title-lg";
+type TypographyBodyKey = "body-xs" | "body-sm" | "body-md" | "body-lg";
+
+type TypographyLevel =
+    | TypographyBodyKey
+    | TypographyTitleKey
+    | TypographyHeadingKey
+    | TypographyDisplayKey;
+
+interface TypographyLevelObj {
+    fontSize: number;
+    lineHeight: string | number;
+    fontWeight: string | number;
+    letterSpacing: string | number;
+}
+
+// API Types
+export type APIUserSettings = {
+    currentTheme?: string | null;
+    currentIcon?: string | null;
+    preferredMode: AppMode;
+    preferEmbossed: boolean;
+    spacePositions: Snowflake[];
+    updatedAt: Date;
+};
+
+export type APIPrivateUser = {
+    id: Snowflake;
+    username: string;
+    defaultAvatar: {
+        type: number;
+        color?: string | null;
+        adapt?: boolean;
+    };
+    previousAvatars: string[];
+    email: string;
+    flags: bigint;
+    globalName?: string | null;
+    dateOfBirth: string;
+    avatar?: string | null;
+    accentColor: string;
+    createdAt: Date;
+    updatedAt: Date;
+};
+
+export type APISpacePartial = Pick<
+    APISpace,
+    "id" | "name" | "icon" | "description"
+>;
+
+export type APIMessageEmbed = {
+    title?: string;
+    description?: string;
+    url?: string;
+    color?: string;
+    timestamp?: number;
+    author?: {
+        name: string;
+        url?: string;
+        iconUrl?: string;
+    };
+    footer?: {
+        text: string;
+        iconUrl?: string;
+    };
+    spotify?: {
+        type: "track" | "album" | "artist" | "playlist";
+        id: Snowflake;
+        embedUrl: string;
+    };
+    youtube?: {
+        videoId: string;
+        embedUrl: string;
+    };
+    apple?: {
+        id: Snowflake;
+        type: "album" | "playlist" | "song" | "artist";
+        embedUrl: string;
+    };
+    image?: string;
+    media?: string;
+    thumbnail?: string;
+    spoiler?: boolean;
+};
+
+export type APIUser = Omit<
+    APIPrivateUser,
+    "email" | "settings" | "previousAvatars" | "dateOfBirth"
+>;
+
+export type APIChannelPermissionOverwrite = {
+    channelId: Snowflake;
+    spaceId: Snowflake;
+
+    roleId?: Snowflake | null;
+    userId?: Snowflake | null;
+
+    allow: bigint;
+    deny: bigint;
+
+    createdAt: Date;
+    updatedAt: Date;
+};
+
+export type APISpace = {
+    id: Snowflake;
+    name: string;
+    ownerId: Snowflake;
+    owner?: APIUser | null;
+    flags: bigint;
+    description?: string | null;
+    icon?: string | null;
+    vanityCode?: string | null;
+    members?: APISpaceMember[] | null;
+    channels?: APIChannel[] | null;
+    roles?: APIRole[] | null;
+    everyoneRoleId: Snowflake;
+    memberCount: number;
+    createdAt: Date;
+    updatedAt: Date;
+};
+
+export type APIRole = {
+    id: Snowflake;
+    name: string;
+    spaceId: Snowflake;
+    space?: APISpace | null;
+    color: string;
+    permissions: bigint;
+    position: number;
+    hoist: boolean;
+    flags: bigint;
+    mentionable: boolean;
+    createdAt: Date;
+    updatedAt: Date;
+};
+
+export type APIMemberRole = {
+    spaceId: Snowflake;
+    userId: Snowflake;
+    roleId: Snowflake;
+    role?: APIRole | null;
+};
+
+export type APIInvite = {
+    id: Snowflake;
+    type: InviteType;
+
+    code: string;
+
+    spaceId?: Snowflake | null;
+    space?: APISpace | null;
+
+    channelId?: Snowflake | null;
+    channel?: APIChannel | null;
+
+    userId?: Snowflake | null;
+    user?: APIUser | null;
+
+    inviterId: Snowflake;
+    inviter?: APIUser | null;
+
+    maxUses: number;
+    uses: number;
+
+    createdAt: Date;
+    updatedAt: Date;
+    expiresAt?: Date | null;
+
+    approximateMemberCount?: number | null;
+    approximateActiveCount?: number | null;
+};
+
+export type APIExpression = {
+    id: Snowflake;
+    type: number;
+    name: string;
+    assetHash: string;
+    authorId: Snowflake;
+    spaceId?: Snowflake | null;
+    animated: boolean;
+    flags: bigint;
+    createdAt: Date;
+};
+
+export type APIChannel = {
+    id: Snowflake;
+    type: ChannelType;
+
+    spaceId?: Snowflake | null;
+    space?: APISpace | null;
+
+    name?: string | null;
+
+    ownerId?: Snowflake | null;
+    owner?: APIUser | null;
+
+    topic?: string | null;
+    position: number;
+
+    parentId?: Snowflake | null;
+    parent?: APIChannel | null;
+
+    recipientIds?: Snowflake[];
+    recipients?: APIUser[];
+
+    messages?: APIMessage[] | null;
+
+    lastMessageId?: Snowflake | null;
+    lastMessage?: APIMessage | null;
+
+    nsfw: boolean;
+
+    overwrites?: APIChannelPermissionOverwrite[] | null;
+
+    flags: bigint;
+
+    createdAt: Date;
+    updatedAt: Date;
+
+    icon?: string | null;
+};
+
+export type APIMessage = {
+    id: Snowflake;
+    type: MessageType;
+
+    channelId: Snowflake;
+    channel?: APIChannel | null;
+
+    spaceId?: Snowflake | null;
+    space?: APISpace | null;
+
+    content?: string | null;
+    createdAt: Date;
+
+    authorId: Snowflake;
+    author?: APIUser | null;
+
+    memberId?: Snowflake | null;
+    member?: APISpaceMember | null;
+
+    embeds: APIMessageEmbed[];
+    edited: boolean;
+    updatedAt?: Date;
+    nonce?: Snowflake | null;
+};
+
+export type APISpaceMember = {
+    spaceId: Snowflake;
+    space?: APISpace | null;
+
+    userId: Snowflake;
+    user?: APIUser | null;
+
+    flags: bigint;
+    nickname?: string | null;
+    avatar?: string | null;
+    banner?: string | null;
+
+    roles?: APIMemberRole[];
+
+    joinedAt: Date;
+    updatedAt: Date;
+};
+
+export type APITheme = {
+    id: Snowflake;
+    name: string;
+    description?: string | null;
+    adaptive: boolean;
+    type: ThemeType;
+    style: ThemeStyle;
+    colors: {
+        common: {
+            white: ColorLike;
+            black: ColorLike;
+        };
+
+        // Base colors
+        primary: ColorLike;
+        neutral: ColorLike;
+        background: ColorLike;
+        surface: ColorLike;
+
+        // Feedback colors
+        danger: ColorLike;
+        warning: ColorLike;
+        info: ColorLike;
+        success: ColorLike;
+    };
+
+    typography: {
+        fontFamily: string;
+        colors: {
+            primary: ColorLike;
+            secondary: ColorLike;
+            accent: ColorLike;
+            muted: ColorLike;
+        };
+        levels: Record<TypographyLevel, TypographyLevelObj>;
+    };
+
+    createdAt?: Date;
+    updatedAt?: Date;
+
+    authorId?: Snowflake | null;
+    author?: APIUser | null;
+};
--- src/gateway/index.ts
+++ src/gateway/index.ts
@@ -1,1 +1,140 @@
-export * from "./v1";
+import type {
+    APIExpression,
+    APIPrivateUser,
+    APISpace,
+    APISpaceMember,
+    APITheme,
+    APIUserSettings,
+} from "../api";
+import type { PresencePayload } from "../presence";
+
+export const GatewayOpcodes = {
+    Dispatch: 0,
+    Heartbeat: 1,
+    Identify: 2,
+    Resume: 3,
+    Reconnect: 4,
+    InvalidSession: 5,
+    Hello: 6,
+    HeartbeatAck: 7,
+    System: 8,
+    LazyRequest: 9,
+    PresenceUpdate: 10,
+    PresenceScheduleSet: 11,
+    PresenceScheduleClear: 12,
+    VoiceStateUpdate: 13,
+} as const;
+
+export const GatewayDispatchEvents = {
+    Ready: "READY",
+    Resume: "RESUME",
+
+    // Users
+    UserUpdate: "USER_UPDATE",
+    UserSettingsUpdate: "USER_SETTINGS_UPDATE",
+
+    // Spaces
+    SpaceCreate: "SPACE_CREATE",
+    SpaceDelete: "SPACE_DELETE",
+    SpaceUpdate: "SPACE_UPDATE",
+
+    // Members
+    SpaceMemberAdd: "SPACE_MEMBER_ADD",
+    SpaceMemberRemove: "SPACE_MEMBER_REMOVE",
+    SpaceMemberUpdate: "SPACE_MEMBER_UPDATE",
+    SpaceMemberListUpdate: "SPACE_MEMBER_LIST_UPDATE",
+
+    // Member Roles
+    SpaceMemberRoleAdd: "SPACE_MEMBER_ROLE_ADD",
+    SpaceMemberRoleRemove: "SPACE_MEMBER_ROLE_REMOVE",
+
+    // Channels
+    ChannelCreate: "CHANNEL_CREATE",
+    ChannelUpdate: "CHANNEL_UPDATE",
+    BulkChannelUpdate: "BULK_CHANNEL_UPDATE",
+    BulkChannelDelete: "BULK_CHANNEL_DELETE",
+    ChannelDelete: "CHANNEL_DELETE",
+
+    // Messages
+    MessageCreate: "MESSAGE_CREATE",
+    MessageUpdate: "MESSAGE_UPDATE",
+    MessageDelete: "MESSAGE_DELETE",
+    MessageDeleteBulk: "MESSAGE_DELETE_BULK",
+
+    // Invites
+    InviteCreate: "INVITE_CREATE",
+    InviteUpdate: "INVITE_UPDATE",
+    InviteDelete: "INVITE_DELETE",
+
+    // Roles
+    RoleCreate: "ROLE_CREATE",
+    RoleUpdate: "ROLE_UPDATE",
+    RoleDelete: "ROLE_DELETE",
+
+    // Presence
+    PresenceUpdate: "PRESENCE_UPDATE",
+    PresenceScheduleUpdate: "PRESENCE_SCHEDULE_UPDATE",
+
+    // Voice
+    VoiceServerUpdate: "VOICE_SERVER_UPDATE",
+    VoiceStateSync: "VOICE_STATE_SYNC",
+    VoiceStateUpdate: "VOICE_STATE_UPDATE",
+
+    // Expressions
+    ExpressionCreate: "EXPRESSION_CREATE",
+    ExpressionUpdate: "EXPRESSION_UPDATE",
+    ExpressionDelete: "EXPRESSION_DELETE",
+} as const;
+
+export type EVENT = keyof typeof GatewayDispatchEvents;
+
+export const GatewayCloseCodes = {
+    UnknownError: 1000,
+    InvalidConnection: 1008,
+    SessionTimedOut: 4000,
+    InvalidSession: 4001,
+    NotAuthenticated: 4002,
+    RateLimit: 4008,
+    MissingPermissions: 4009,
+};
+
+export interface GatewayPayload {
+    op: keyof typeof GatewayOpcodes;
+    d?: any;
+    s?: number;
+    t?: keyof typeof GatewayDispatchEvents;
+}
+
+export interface WireGatewayPayload {
+    op: (typeof GatewayOpcodes)[keyof typeof GatewayOpcodes];
+    d?: any;
+    s?: number;
+    t?: (typeof GatewayDispatchEvents)[keyof typeof GatewayDispatchEvents];
+}
+
+export interface GatewaySession {
+    userId: string;
+    lastUsedAt: number;
+    seq: number;
+}
+
+export interface BaseEvent<T extends EVENT = EVENT, D = any> {
+    space_id?: string | null;
+    user_id?: string | null;
+    channel_id?: string | null;
+    event: T;
+    data: D;
+}
+
+export type GatewayReadyPayload = {
+    sessionId: string;
+    user: APIPrivateUser;
+    themes: APITheme[];
+    spaces: APISpace[];
+    settings: APIUserSettings;
+    expressions: APIExpression[];
+};
+
+export interface GatewaySpaceMember extends APISpaceMember {
+    presence?: PresencePayload;
+}
